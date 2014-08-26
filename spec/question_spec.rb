495bcda8fdda99456d29776cc7c89a8b2c923214
Linda Gibson & Marina Neumann <student@epicodus.com>
2014-08-26 14:03:38 -0700

    establish association where a question has many responses.

--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -6,4 +6,11 @@ describe Question do
     test_question = Question.create({:name => 'Did you eat breakfast?', :survey_id => test_survey.id })
     expect(test_question.survey).to eq test_survey
   end
+
+  it 'has many responses' do
+    test_question = Question.create({:name => 'What is the best Mexican restaurant?'})
+    test_response1 = Response.create({:name => 'Don Pedro', :question_id => test_question.id})
+    test_response2 = Response.create({:name => 'Por Que No?', :question_id => test_question.id})
+    expect(test_question.responses).to eq [test_response1, test_response2]
+  end
 end
