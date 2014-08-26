cb6892d85d2811579a0a4120f7c8558a8486074b
Linda Gibson & Marina Neumann <student@epicodus.com>
2014-08-26 15:10:21 -0700

    Add association that a choice can have many responses.

--- spec/choice_spec.rb
+++ spec/choice_spec.rb
@@ -6,4 +6,11 @@ describe Choice do
     test_choice = Choice.create({:name => 'To enjoy every candy bar!', :question_id => test_question.id})
     expect(test_choice.question).to eq test_question
   end
+
+  it 'has many responses' do
+    test_question = Question.create({:name => 'What is the meaning of life?'})
+    test_choice = Choice.create({:name => 'To be happy :)'})
+    test_response = Response.create({:question_id => test_question.id, :choice_id => test_choice.id})
+    expect(test_choice.responses).to eq [test_response]
+  end
 end
