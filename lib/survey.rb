f75a0b15a7784da17e34a1cc2c9d9a6b20e6302a
Linda Gibson & Marina Neumann <student@epicodus.com>
2014-08-26 11:18:23 -0700

    Establish association for a survey that has many questions.

--- lib/survey.rb
+++ lib/survey.rb
@@ -1,7 +1,5 @@
 class Survey < ActiveRecord::Base
 
   has_many :questions
-
-
 end
 
--- spec/survey_spec.rb
+++ spec/survey_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe Survey do
   it 'has_many questions' do
     test_survey = Survey.create({:name => 'Are you hungry?'})
-    test_question1 = Question.create({:name => 'Did you eat breakfast?'})
-    test_question2 = Question.create({:name => 'Is your stomach growling?'})
+    test_question1 = Question.create({:name => 'Did you eat breakfast?', :survey_id => test_survey.id})
+    test_question2 = Question.create({:name => 'Is your stomach growling?', :survey_id => test_survey.id})
     expect(test_survey.questions).to eq [test_question1, test_question2]
   end
 end
