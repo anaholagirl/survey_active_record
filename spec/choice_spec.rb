8eed263851e8e27debeb1e9605240827433d2df5
Linda Gibson & Marina Neumann <student@epicodus.com>
2014-08-26 15:02:59 -0700

    Adds association that a choice belongs to a question.

--- spec/choice_spec.rb
+++ spec/choice_spec.rb
@@ -1,2 +1,9 @@
 require 'spec_helper'
 
+describe Choice do
+  it 'belongs to a question' do
+    test_question = Question.create({:name => 'What is the meaning of life?'})
+    test_choice = Choice.create({:name => 'To enjoy every candy bar!', :question_id => test_question.id})
+    expect(test_choice.question).to eq test_question
+  end
+end
